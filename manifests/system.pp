6fbccb6296db1c24000e96eca7b4fd4387eaca9e
Justin Dray <justin@dray.be>
2014-10-01 09:10:24 +1000

    Added lots of options for system level btsync

--- manifests/system.pp
+++ manifests/system.pp
@@ -1,5 +1,21 @@
-class btsync::system( $webui = 'local' ) {
+class btsync::system( $listening_port = 0,
+                      $storage_path = '/var/lib/btsync',
+                      $use_upnp = true,
+                      $download_limit = 0,
+                      $upload_limit = 0,
+                      $webui_listen = '127.0.0.1:8888',
+                      $login = undef,
+                      $password = undef,
+                      $directory_root = "/",) {
   include btsync
+
+  validate_re($listening_port, '\d+')
+  validate_absolute_path($storage_path)
+  validate_bool($use_upnp)
+  validate_re($download_limit, '\d+')
+  validate_re($upload_limit, '\d+')
+  validate_absolute_path($directory_root)
+
 
   service { 'btsync':
     ensure => running,
