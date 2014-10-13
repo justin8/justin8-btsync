8d5d2904053cf36ec79f997ca673e4818518350c
Justin Dray <justin@dray.be>
2014-10-13 16:51:44 +1000

    Added tmpfiles.d configuration to allow non-btsync users to run as a folder service

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,15 @@ class btsync( $webui = 'local' ) {
     ensure => present,
   }
 
-  file { '/var/lib/btsync/custom':
-    ensure => directory,
-    purge  => true,
-    force  => true,
+  file {
+    '/var/lib/btsync/custom':
+      ensure => directory,
+      purge  => true,
+      force  => true;
+
+    '/usr/lib/tmpfiles.d/btsync.conf':
+      ensure => file,
+      source => 'puppet:///modules/btsync/btsync.conf';
   }
 
 }
