6be55dcfcf0b4a23fb45b7aea5f9a3fff48621aa
Justin Dray <justin@dray.be>
2015-08-31 19:21:57 +1000

    Updated to support OSes without a packaged btsync

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,21 @@
 class btsync( $webui = 'local' ) {
   include systemd
 
-  package { 'btsync':
-    ensure => present,
+  if $::operatingsystem == 'Archlinux' {
+    ensure_packages(['btsync'])
+  } else {
+    ensure_packages(['curl'])
+
+    exec { 'download-btsync':
+      command => '/usr/bin/curl -o /tmp/btsync.tar.gz https://download-cdn.getsync.com/stable/linux-x64/BitTorrent-Sync_x64.tar.gz',
+    }~>
+    exec { 'extract-btsync':
+      command => 'cd /usr/bin; tar xf /tmp/btsync.tar.gz btsync',
+    }
+
+    file { '/etc/systemd/system/btsync.service':
+      source => 'puppet:///modules/btsync/btsync.service',
+    }
   }
 
   file {
