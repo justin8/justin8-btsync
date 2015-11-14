b399c8bb9a10f6a66e3c8cba000f99c6ae1213b2
Justin Dray <justin@dray.be>
2015-11-14 01:42:11 +0100

    Add umask option

--- manifests/folder.pp
+++ manifests/folder.pp
@@ -4,42 +4,37 @@ define btsync::folder(
   $path = $title,
   $owner = 'root',
   $group = 'root',
+  $umask = '0002',
   $listening_port = 0,
   $use_upnp = true,
   $download_limit = 0,
   $upload_limit = 0,
-  $use_relay_server = false,
-  $use_tracker = false,
-  $use_dht = true,
-  $search_lan = true,
-  $sync_trash = true,
-  $overwrite_changes = false,
   $proxy = false,
   $proxy_type = 'socks4',
   $proxy_addr = '127.0.0.1',
   $proxy_port = '8080',
   $proxy_username = '',
   $proxy_password = '',
+  $use_relay_server = false,
+  $use_tracker = false,
+  $use_dht = true,
+  $search_lan = true,
+  $sync_trash = true,
+  $overwrite_changes = false,
   $ignore_list = [])  {
   include btsync
+
+  Exec {
+    path => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin'
+  }
 
   validate_absolute_path($path)
   validate_bool($use_upnp, $use_relay_server, $use_tracker, $use_dht, $search_lan, $sync_trash, $overwrite_changes)
   validate_re($listening_port, '\d+')
   validate_re($download_limit, '\d+')
   validate_re($upload_limit, '\d+')
+  validate_re($umask, '\d{4}')
 
-  if ! is_numeric($listening_port) {
-    fail("Btsync::Folder[${title}]: listening_port should be a number")
-  }
-
-  if ! is_numeric($download_limit) {
-    fail("Btsync::Folder[${title}]: download_limit should be a number")
-  }
-
-  if ! is_numeric($upload_limit) {
-    fail("Btsync::Folder[${title}]: upload_limit should be a number")
-  }
 
   $int_path = regsubst($path, '/', '')
   $clean_path = regsubst($int_path, '/', '-', 'G')
--- manifests/system.pp
+++ manifests/system.pp
@@ -1,25 +1,34 @@
-class btsync::system( $listening_port = 0,
-                      $storage_path = '/var/lib/btsync/system',
-                      $user = 'btsync',
-                      $group = 'btsync',
-                      $use_upnp = true,
-                      $download_limit = 0,
-                      $upload_limit = 0,
-                      $webui_listen = '127.0.0.1:8888',
-                      $login = undef,
-                      $password = undef,
-                      $directory_root = '/',) {
+class btsync::system(
+  $listening_port = 0,
+  $storage_path = '/var/lib/btsync/system',
+  $user = 'btsync',
+  $group = 'btsync',
+  $umask = '0002',
+  $use_upnp = true,
+  $download_limit = 0,
+  $upload_limit = 0,
+  $proxy = false,
+  $proxy_type = 'socks4',
+  $proxy_addr = '127.0.0.1',
+  $proxy_port = '8080',
+  $proxy_username = '',
+  $proxy_password = '',
+  $webui_listen = '127.0.0.1:8888',
+  $login = undef,
+  $password = undef,
+  $directory_root = '/',) {
   contain btsync
 
   Exec {
     path => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin'
   }
 
-  validate_re($listening_port, '\d+')
   validate_absolute_path($storage_path)
   validate_bool($use_upnp)
+  validate_re($listening_port, '\d+')
   validate_re($download_limit, '\d+')
   validate_re($upload_limit, '\d+')
+  validate_re($umask, '\d{4}')
   validate_absolute_path($directory_root)
 
 
